6fc132c44731ced0c8f30e60d7c5106db949a9c7
Anturia Hassani <anturia.hm@gmail.com>
2025-07-06 14:00:57 +0200

    Route api/books

--- app.js
+++ app.js
@@ -2,23 +2,42 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue'});
-    next();
-});
-
-app.use((req, res,) => {
-    console.log('Réponse envoyée avec succès !');
-});
+// app.use('api/books', (req, res, next) => {
+//     const books = [
+//         {
+//             _id: 'book1',
+//             title: 'Titre 1',
+//             author: 'Auteur 1',
+//             description: 'Description 1',
+//             imageUrl: 'https://cdn.pixabay.com/photo/2016/11/18/56/camera-4267692_1280.jpg',
+//             year: 2025,
+//             genre: 'Genre 1',
+//             ratings: [
+//                 {
+//                     userId: 'Commentateur 1',
+//                     grade: 3,
+//                 }
+//             ],
+//             averageRating: 3,
+//         },
+//         {
+//             _id: 'book2',
+//             title: 'Titre 2',
+//             author: 'Auteur 2',
+//             description: 'Description 2',
+//             imageUrl: 'https://image-url.com/hp1.jpg',
+//             year: 2025,
+//             genre: 'Genre 2',
+//             ratings: [
+//                 {
+//                     userId: 'Commentateur 2',
+//                     grade: 4,
+//                 }
+//             ],
+//             averageRating: 4,
+//         },
+//     ];
+//     res.status(200).json(books);
+// });
 
 module.exports = app;
