d3ac1ac5fd730afda5e30c1c154bc13c344a61ed
Anturia Hassani <anturia.hm@gmail.com>
2025-07-09 11:35:34 +0200

    Etape 1.1 terminée

--- app.js
+++ app.js
@@ -2,7 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use('/api/books', (req, res, next) => {
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.post('/api/books', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Objet crée !'
+    });
+}); 
+
+app.get('/api/books', (req, res, next) => {
     const books = [
         {
             _id: 'book1',
