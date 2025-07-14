c0ac7a68f889414345ad674e422be46703c9012a
Anturia Hassani <anturia.hm@gmail.com>
2025-07-14 23:12:28 +0200

    Modif createBook + modifyBook

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
 
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
 
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
 module.exports = app;
