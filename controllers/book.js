14f87dcaeedc84a9a4cf2b73db03182c79acde40
Anturia Hassani <anturia.hm@gmail.com>
2025-07-15 23:13:30 +0200

    Optimisation fichiers

--- controllers/book.js
+++ controllers/book.js
@@ -1,73 +1,128 @@
 const Book = require('../models/Book');
-const fs = require('fs');
+const fs = require('fs/promises');
+const sharp = require('sharp');
+const path = require('path');
 
-exports.createBook = (req, res, next) => {
-   const bookObject = JSON.parse(req.body.book);
-   delete bookObject._id;
-   delete bookObject._userId;
-   const book = new Book({
-       ...bookObject,
-       userId: req.auth.userId,
-       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-   });
- 
-   book.save()
-   .then(() => { res.status(201).json({message: 'Livre enregistré !'})})
-   .catch(error => { res.status(400).json( { error })})
+exports.createBook = async (req, res, next) => {
+  try {
+    const bookObject = JSON.parse(req.body.book);
+    delete bookObject._id;
+    delete bookObject._userId;
+
+    const inputPath = req.file.path;          
+    const filename = req.file.filename.replace(/\.[^.]+$/, '.webp');
+    const outputPath = path.join(path.dirname(inputPath), filename);
+
+    await sharp(inputPath)
+      .resize({ width: 800, withoutEnlargement: true })
+      .webp()
+      .toFile(outputPath);
+
+    await fs.unlink(inputPath);                
+
+    const book = new Book({
+      ...bookObject,
+      userId: req.auth.userId,
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${filename}`
+    });
+
+    await book.save();
+    res.status(201).json({ message: 'Livre enregistré !' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
 };
 
-exports.modifyBook = (req, res, next) => {
-    const bookObject = req.file ? {
+
+exports.modifyBook = async (req, res, next) => {
+  try {
+    const bookId = req.params.id;
+    let bookObject = {};
+
+    if (req.file) {
+
+      const inputPath = req.file.path;
+      const filename = req.file.filename.replace(/\.[^.]+$/, '.webp');
+      const outputPath = path.join(path.dirname(inputPath), filename);
+
+      await sharp(inputPath)
+        .resize({ width: 800, withoutEnlargement: true })
+        .webp()
+        .toFile(outputPath);
+
+      await fs.unlink(inputPath); 
+
+      bookObject = {
         ...JSON.parse(req.body.book),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : { ...req.body };
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${filename}`
+      };
+    } else {
+      bookObject = { ...req.body };
+    }
 
     delete bookObject._userId;
-    Book.findOne({_id: req.params.id})
-    .then((book) => {
-        if (book.userId != req.auth.userId) {
-            res.status(400).json({ message : 'Non-autorisé' });
-        } else {
-            Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
-            .then(() => res.status(200).json({message: 'Livre modifié !'}))
-            .catch(error => res.status(401).json({ error }));
-        }
-    })
-    .catch((error) => {
-            res.status(400).json({ error });
-        });
-    
-}
+
+    const book = await Book.findOne({ _id: bookId });
+
+    if (!book) {
+      return res.status(404).json({ message: 'Livre non trouvé' });
+    }
+
+    if (book.userId != req.auth.userId) {
+      return res.status(403).json({ message: 'Non autorisé' });
+    }
+
+    // Supprime l'ancienne image du serveur si nouvelle image envoyée
+    if (req.file && book.imageUrl) {
+      const oldImageFilename = book.imageUrl.split('/images/')[1];
+      const oldImagePath = path.join(__dirname, '../images', oldImageFilename);
+
+      try {
+        await fs.unlink(oldImagePath);
+      } catch (error) {
+        console.warn('Ancienne image non supprimée (peut-être déjà absente) :', error.message);
+      }
+    }
+
+    await Book.updateOne({ _id: bookId }, { ...bookObject, _id: bookId });
+    res.status(200).json({ message: 'Livre modifié !' });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+};
 
 exports.deleteBook = (req, res, next) => {
-    Book.findOne({ _id: req.params.id})
-    .then(book => {
-        if (book.userId != req.auth.userId) {
-            res.status(401).json({message: 'Non-autorisé'});
-        } else {
-            const filename = book.imageUrl.split('/images/')[1];
-            fs.unlink(`images/${filename}`, () => {
-                Book.deleteOne({_id: req.params.id})
-                .then(() => { res.status(200).json({message: 'Livre supprimé !'})})
-                .catch(error => res.status(401).json({ error }));
-            });
-        }
-    })  
-    .catch( error => {
-        res.status(500).json({ error });
-    });
+    Book.findOne({ _id: req.params.id })
+        .then(book => {
+            if (book.userId != req.auth.userId) {
+                res.status(401).json({ message: 'Non-autorisé' });
+            } else {
+                const filename = book.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Book.deleteOne({ _id: req.params.id })
+                        .then(() => { res.status(200).json({ message: 'Livre supprimé !' }) })
+                        .catch(error => res.status(401).json({ error }));
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error });
+        });
 }
 
 
 exports.getOneBook = (req, res, next) => {
     Book.findOne({ _id: req.params.id })
-    .then(book => res.status(200).json(book))
-    .catch(error => res.status(404).json({ error }));
+        .then(book => res.status(200).json(book))
+        .catch(error => res.status(404).json({ error }));
 }
 
-exports.getAllBooks =  (req, res, next) => {
-   Book.find()
-   .then(books => res.status(200).json(books))
-   .catch(error => res.status(400).json({ error }));
+exports.getAllBooks = (req, res, next) => {
+    Book.find()
+        .then(books => res.status(200).json(books))
+        .catch(error => res.status(400).json({ error }));
 }
 
